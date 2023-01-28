b0bfa28e1bf9c326eec6c75e758e2530fba50659
raczko <91797787+raczko@users.noreply.github.com>
2023-01-28 14:43:11 +0100

    Add files via upload

---
+++ Fasada/Fasada/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using SystSystemem.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WzorzecFasada
+{
+
+    interface IUserService
+    {
+        void CreateUser(string email);
+        void DeleteUser(string email);
+        void UserCount();
+    }
+
+    static class EmailNotification
+    {
+        public static void SendEmail(string to, string subject)
+        {
+            Console.WriteLine($"{subject}{to}");
+        }
+    }
+
+    class UserRepository
+    {
+        private readonly List<string> users = new List<string>
+        {
+            "john.doe@gmail.com", "sylvester.stallone@gmail.com"
+        };
+
+        public int UsersCount()
+        {
+            return users.Count;
+        }
+
+        public bool IsEmailFree(string email)
+        {
+
+            return !users.Contains(email);
+        }
+
+
+        public void AddUser(string email)
+        {
+            users.Add(email);
+        }
+
+        public void DeleteUser(string email)
+        {
+            users.Remove(email);
+        }
+    }
+
+    static class Validators
+    {
+        public static bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase);
+        }
+    }
+
+    class UserService : IUserService
+    {
+        private readonly UserRepository userRepository = new UserRepository();
+        public void CreateUser(string email)
+        {
+            if (!Validators.IsValidEmail(email))
+            {
+                throw new ArgumentException("Błędny email");
+            }
+
+            if (!userRepository.IsEmailFree(email))
+            {
+                throw new ArgumentException("Ktoś ma już taki email");
+            }
+
+            userRepository.AddUser(email);
+            EmailNotification.SendEmail(email, "Welcome to our service ");
+        }
+        public void DeleteUser(string email)
+        {
+            if (!Validators.IsValidEmail(email))
+            {
+                throw new ArgumentException("Błędny email");
+            }
+            if (userRepository.IsEmailFree(email))
+            {
+                throw new ArgumentException("Nie ma takiego użytkownika");
+            }
+
+            userRepository.DeleteUser(email);
+            EmailNotification.SendEmail(email, "Goodbye ");
+        }
+        public void UserCount()
+        {
+            Console.WriteLine("Aktualna liczba adresów: " + userRepository.UsersCount());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUserService userService = new UserService();
+            userService.UserCount();
+            userService.CreateUser("someemail@gmail.com");
+            userService.UserCount();
+            userService.DeleteUser("john.doe@gmail.com");
+            userService.UserCount();
+        }
+    }
+
+}
---
+++ Most/Most/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+
+public interface ITelewizor
+{
+
+    int Kanal { get; set; }
+    void Wlacz();
+    void Wylacz();
+    void ZmienKanal(int kanal);
+
+}
+
+
+
+public class TvLg : ITelewizor
+{
+
+    public TvLg()
+    {
+        this.Kanal = 1;
+    }
+
+    public int Kanal { get; set; }
+
+    public void Wlacz()
+    {
+        Console.WriteLine("Telewizor LG - włączam się.");
+    }
+
+    public void Wylacz()
+    {
+        Console.WriteLine("Telewizor LG - wyłączam się.");
+    }
+
+    public void ZmienKanal(int kanal)
+    {
+        this.Kanal = kanal;
+        Console.WriteLine($"Telewizor LG - zmieniam kanał: {kanal}");
+    }
+
+}
+
+
+
+public class TvXiaomi : ITelewizor
+{
+    public TvXiaomi()
+    {
+        this.Kanal = 1;
+    }
+
+    public int Kanal { get; set; }
+
+    public void Wlacz()
+    {
+        Console.WriteLine("Telewizor Xiaomi - włączam się.");
+    }
+
+    public void Wylacz()
+    {
+        Console.WriteLine("Telewizor Xiaomi - wyłączam się.");
+    }
+
+    public void ZmienKanal(int kanal)
+    {
+        Kanal = kanal;
+        Console.WriteLine($"Telewizor Xiaomi - zmieniam kanał: {kanal}");
+    }
+}
+
+
+
+public abstract class PilotAbstrakcyjny
+{
+
+    private ITelewizor tv;
+
+    public PilotAbstrakcyjny(ITelewizor tv)
+    {
+        this.tv = tv;
+    }
+    public void Wlacz()
+    {
+        tv.Wlacz();
+    }
+    public void Wylacz()
+    {
+        tv.Wylacz();
+    }
+
+    public void ZmienKanal(int kanal)
+    {
+        tv.ZmienKanal(kanal);
+    }
+
+}
+
+
+
+public class PilotHarmony : PilotAbstrakcyjny
+{
+
+    public PilotHarmony(ITelewizor tv) : base(tv) { }
+
+    public void DoWlacz()
+    {
+        Console.WriteLine("Pilot Harmony - włącz telewizor...");
+        Wlacz();
+    }
+    public void DoWylacz()
+    {
+        Console.WriteLine("Pilot Harmony - wyłącz telewizor...");
+        Wylacz();
+    }
+    public void DoZmienKanal(int kanal)
+    {
+        Console.WriteLine($"Pilot Harmony - zmienia kanał: {kanal} ");
+        ZmienKanal(kanal);
+
+    }
+
+}
+
+public class PilotPhilips : PilotAbstrakcyjny
+{
+
+    public PilotPhilips(ITelewizor tv) : base(tv) { }
+
+    public void DoWlacz()
+    {
+        Console.WriteLine("Pilot Philips - włącz telewizor...");
+        Wlacz();
+    }
+    public void DoWylacz()
+    {
+        Console.WriteLine("Pilot Philips - wyłącz telewizor...");
+        Wylacz();
+    }
+    public void DoZmienKanal(int kanal)
+    {
+        Console.WriteLine("Pilot Philips - zmienia kanał... ");
+        ZmienKanal(kanal);
+    }
+
+}
+
+public class PilotLG : PilotAbstrakcyjny
+{
+
+    public PilotLG(ITelewizor tv) : base(tv) { }
+
+    public void DoWlacz()
+    {
+        Console.WriteLine("Pilot LG - - włącz telewizor...");
+        Wlacz();
+    }
+    public void DoWylacz()
+    {
+        Console.WriteLine("Pilot LG - - wyłącz telewizor...");
+        Wylacz();
+    }
+    public void DoZmienKanal(int kanal)
+    {
+        Console.WriteLine("Pilot LG - zmienia kanał... ");
+        ZmienKanal(kanal);
+    }
+
+}
+
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        ITelewizor tv = new TvLg();
+        PilotHarmony pilotHarmony = new PilotHarmony(tv);
+        PilotLG pilotLG = new PilotLG(tv);
+
+        pilotHarmony.DoWlacz();
+        Console.WriteLine("Sprawdź kanał - bieżący kanał: " + tv.Kanal);
+        pilotLG.DoZmienKanal(100);
+        Console.WriteLine("Sprawdź kanał - bieżący kanał: " + tv.Kanal);
+        pilotHarmony.DoWylacz();
+
+    }
+}
